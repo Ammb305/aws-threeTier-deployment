0ba9403f9899ff1fba1c19befb4e8ded5de44092
Ammb305 <63208139+Ammb305@users.noreply.github.com>
2024-11-04 20:17:59 +0200

    Updated

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: 'terraform-20241027185710975700000001.c5g22ewq4ztf.us-east-1.rds.amazonaws.com',
+   host: 'terraform-20241104123538121200000003.c5g22ewq4ztf.us-east-1.rds.amazonaws.com',
    port: '3306',
    user: 'admin',
    password: 'admin123',
-   database: 'react_node_app'
+   database: 'MySQLdatabase'
 });
 
 module.exports = db;
